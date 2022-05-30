0b240b103b2c13785ee8b94d9326571294452273
RaphaelDev11 <hhj91350@gmail.com>
2022-05-30 12:41:40 +0200

    initial commit

---
+++ src/components/BlockImageRight.vue
@@ -0,0 +1,29 @@
+<template>
+        <div class="container" id="1"> <!---5th container---->
+            <div class="main-block1 row"> 
+
+                <div class="main-block1l col-md"> <!-----left---->
+                    <div class="heading-info"><h1 class="text2"> {{titre}}</h1></div>
+                    <div class=""><p class="text3">{{text}}</p></div>
+                    <div class="btn1-container"><button class="btn btn-lg fs-5" style="background-color: rgb(27, 179, 27); color: white; padding: 20px 25px;">{{btntext}}</button></div>
+                </div>
+
+                <div class="main-block1r col-md"> <!-----right---->
+                    <img src="img/img3.png" alt="" class="img-fluid">
+                </div>
+
+            </div>
+        </div>
+
+</template>
+
+<script>
+export default {
+  name: 'BlockImageRight',
+  props: {
+    titre: String,
+    text: String,
+    btntext: String
+  }
+}
+</script>
---
+++ src/components/MenuMain.vue
@@ -0,0 +1,47 @@
+<template>
+
+
+
+<!-- Navbar + Head Block(Space)  -->
+            <nav class="navbar navbar-expand-lg navbar-light menumain sticky-top">
+                <a class="navbar-brand" href="#"><img src="img/logo.png" class="menulogo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between menumain" id="navbarNav">
+                  <ul class="navbar-nav menuul">
+                      
+                    <li class="nav-item active">
+                      <a class="" href="#1">Logiciel de facturisation gratuit</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="" href="#4">Fonctionalités</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="" href="#3">Tarifs</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="" href="#questions">A propos</a>
+                    </li>
+                  </ul>
+
+                  <ul class="menuul navbar-nav">
+                    <button class="btn btn-light btn2" style="background-color: rgb(255, 255, 255); padding: 4px 16px; margin: 8px; border: 1px solid rgb(197, 194, 194);">Connexion</button>
+                    <button class="btn btn-success btn2" style="background-color: rgb(27, 179, 27); color: white; padding: 4px 16px; margin: 8px;">Créer un compte</button>
+                </ul>
+                </div>
+              </nav>
+              
+ <!--Head Block (Space)--->
+<div class="head-block"></div>
+
+</template>
+
+<script>
+export default {
+  name: 'MenuMain',
+  props: {
+    msg: String
+  }
+}
+</script>
