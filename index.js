1dd11d18c6d946b204398cf20ad5a6f0300cd315
RaphaelDev11 <hhj91350@gmail.com>
2022-07-14 21:12:52 +0200

    make it better

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ const cors = require('cors');
 const port = process.env.PORT || 3000
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
- });
+});
 
 app.use(express.json());
 app.use(cors());
